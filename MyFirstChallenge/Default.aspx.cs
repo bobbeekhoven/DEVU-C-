5f1a4011c9dba8403f6395d20b2407ed802653d9
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-05-15 14:27:35 +0200

    Add files via upload

---
+++ MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ageBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void moneyBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Fortune_Click(object sender, EventArgs e)
+        {
+            string ageBox = ageBoxTextBox.Text;
+            string moneyBox = moneyBoxTextBox.Text;
+
+            string result = "At " + ageBox + " years old, I would have expected you to have more than $" + moneyBox + " in your pocket.";
+
+            resultLabel.Text = result;
+        }
+    }
+}
