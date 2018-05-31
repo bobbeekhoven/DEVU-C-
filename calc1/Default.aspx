d3975d8727277e5c5e613df7fa1eabbbdf055ab4
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-05-31 15:57:27 +0200

    Add files via upload

---
+++ calc1/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="calc1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculater</h1>
+            <p>
+                <span class="auto-style1">value 1</span>:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <span class="auto-style1">value 2</span>:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="+" />
+&nbsp;<asp:Button ID="subtractButton" runat="server" OnClick="subtractButton_Click" Text="-" />
+&nbsp;<asp:Button ID="multiplyButton" runat="server" OnClick="multiplyButton_Click" Text="*" />
+&nbsp;<asp:Button ID="divideButton" runat="server" OnClick="divideButton_Click" Text="/" />
+            </p>
+            <p>
+                <asp:Label ID="resultLabel" runat="server" style="background-color: #00CCFF"></asp:Label>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ calc1/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace calc1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(TextBox1.Text);
+            double second = double.Parse(TextBox2.Text);
+            double result = first + second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(TextBox1.Text);
+            double second = double.Parse(TextBox2.Text);
+            double result = first - second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(TextBox1.Text);
+            double second = double.Parse(TextBox2.Text);
+            double result = first * second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(TextBox1.Text);
+            double second = double.Parse(TextBox2.Text);
+            double result = first / second;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
