fea649060d294895cc54c97cdd87e29170a7ed18
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-06-22 13:37:51 +0200

    Add files via upload

---
+++ CS-ASP_15/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_15
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            // http://is.gd/timespan
+            // Days.Hours:Minutes:Seconds.Miliseconds
+            TimeSpan myTimeSpan = TimeSpan.Parse("1.2:3:30.5");
+
+            DateTime myBirthday = DateTime.Parse("15/2/1979");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            resultLabel.Text = myAge.Hours.ToString();
+
+            resultLabel.Text = myAge.TotalDays.ToString();
+
+        }
+    }
+}
---
+++ CS-ASP_15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS_ASP_15.Global" Language="C#" %>
