ddfa380bf2607185a19c4322baab5cf6d48018ea
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-08-14 15:02:49 +0200

    Add files via upload

---
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeForXmenBattleCount.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: "Comic Sans MS";
+            text-decoration: underline;
+            font-weight: normal;
+            color: #FF0000;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 932px; background-color: #FFFF00">
+    
+        <asp:Image ID="Image1" runat="server" Height="121px" ImageUrl="~/kisspng-professor-x-cyclops-wolverine-x-men-logo-x-men-5ac42d4b9cfbf0.434739291522806091643.jpg" Width="244px" />
+        <h4 class="auto-style1"><em><strong>X-MEN BATTLE COUNTER</strong></em></h4>
+    
+        <asp:Label ID="resultLabel" runat="server" style="font-family: 'Comic Sans MS'; color: #FF0000; font-weight: 700; font-style: italic; text-decoration: underline"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Phoenix most battles
+
+            string[] names = new string[]
+            { "Professor X", "Iceman", "Angel", "Beast",
+                "Phoenix", "Cyclops", "Wolverine",
+                "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+            int largestNumberIndex = 0;
+            int smallestNumberIndex = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // is this the largest count so far?
+                if (numbers[i] >= numbers[largestNumberIndex])
+                {
+                    largestNumberIndex = i;
+                }
+
+                // is this the smallest count so far?
+                if (numbers[i] <= numbers[smallestNumberIndex])
+                {
+                    smallestNumberIndex = i;
+                }
+
+                
+            }
+
+            result = String.Format("Most battles belong to: {0}<br /> (Value: {1})",
+                names[largestNumberIndex], numbers[largestNumberIndex]);
+            result += String.Format("<br />Least battles belong to: {0}<br /> (Value: {1})",
+                names[smallestNumberIndex], numbers[smallestNumberIndex]);
+            result += String.Format("<br />Average amount of battles are:<br /> (Value: {0:N0})",
+                numbers.Average());
+
+            resultLabel.Text = result;
+        }
+    }
+}
