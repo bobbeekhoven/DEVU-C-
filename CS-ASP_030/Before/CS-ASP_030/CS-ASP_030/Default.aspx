aa5259bd05b39cdc47fb45c028e78fc8e8b6ca94
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-09-03 13:47:24 +0200

    Add files via upload

---
+++ CS-ASP_030/Before/CS-ASP_030/CS-ASP_030/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_030.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Image ID="Image1" runat="server" ImageUrl="~/Heroquest_logo.png" Width="507px" />
+        <br />
+        <asp:Image ID="Image2" runat="server" Height="294px" ImageUrl="~/hqcardb.jpg" Width="232px" />
+&nbsp;<asp:Image ID="Image3" runat="server" Height="294px" ImageUrl="~/hqcarda.jpg" Width="232px" />
+        <br />
+        <asp:Button ID="battleButton" runat="server" OnClick="battleButton_Click" style="font-family: 'Comic Sans MS'" Text="Start Battle!!!" />
+&nbsp;<asp:Button ID="clearButton" runat="server" OnClick="clearButton_Click" style="font-family: 'Comic Sans MS'" Text="Clear Battle" />
+        <br />
+    
+        <asp:Label ID="resultLabel" runat="server" style="font-family: 'Comic Sans MS'"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_030/Before/CS-ASP_030/CS-ASP_030/Default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_030
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+        }
+
+        protected void battleButton_Click(object sender, EventArgs e)
+        {
+            int heroHealth = 150;
+            int monsterHealth = 100;
+
+
+            displayBattleHeader();
+
+            // Hero gets bonus first attack 
+            monsterHealth = performAttack(monsterHealth, 50, "Barbarian", "Dark Elf");
+
+            while (heroHealth > 0 && monsterHealth > 0)
+            {
+                displayRoundHeader();
+
+                // Perform battle here!
+                heroHealth = performAttack(heroHealth, 20, "Dark Elf", "Barbarian");
+                monsterHealth = performAttack(monsterHealth, 10, "Barbarian", "Dark Elf");
+
+            }
+
+            displayResult(heroHealth, monsterHealth);
+        }
+
+
+        private void displayBattleHeader()
+        {
+            resultLabel.Text += "<h3>Battle Between the Barbarian (you) and the Dark Elf (bad guy)</h3>";
+        }
+
+        private void displayRoundHeader()
+        {
+            resultLabel.Text += "<hr /><p>Round begins ...</p>";
+        }
+
+        private int performAttack(int defenderHealth, int attackerDamageMax, string attackerName, string defenderName)
+        {
+            Random random = new Random();
+            int damage = random.Next(1, attackerDamageMax);
+            defenderHealth -= damage;
+
+            describeRound(attackerName, defenderName, defenderHealth);
+
+            return defenderHealth;
+        }
+
+        private void describeRound(string attackerName, string defenderName, int defenderHealth)
+        {
+            if (defenderHealth <= 0)
+                resultLabel.Text += String.Format("<br />{0} attacks {1} and vanquishes the {2}.", attackerName, defenderName, defenderName);
+            else
+                resultLabel.Text += String.Format("<br />{0} attacks {1}, leaving {2} with {3} health.", attackerName, defenderName, defenderName, defenderHealth);
+        }
+
+        private void displayResult(int heroHealth, int monsterHealth)
+        {
+            if (heroHealth <= 0)
+                resultLabel.Text += "<h3>Dark Elf wins!</h3>";
+            else if (monsterHealth <= 0)
+                resultLabel.Text += "<h3>Barbarian wins!</h3>";
+            else
+                resultLabel.Text += "<h3>They are both losers!</h3>";
+        }
+
+        protected void clearButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+        }
+    }
+}
