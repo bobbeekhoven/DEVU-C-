26695f1f83d5ee5f965f98bbdd5ab5396a1cec20
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-10-01 12:48:45 +0200

    Add files via upload

---
+++ CS-ASP_037/CS-ASP_037/Domain/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS_ASP_037
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhone { get; set; }
+    }
+}
