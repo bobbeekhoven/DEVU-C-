581fa9e12e57f835ac5f5dcee76f4f1f66aae083
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-10-09 13:18:05 +0200

    Add files via upload

---
+++ CS-ASP_043/CS-ASP_043/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_043
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Car myCar = new Car();
+            Car myCar = new Car("Saab", "9.5", 2001, "Grey");
+            resultLabel.Text = myCar.FormatDetailsForDisplay();
+
+            DateTime d = new DateTime();
+
+        }
+    }
+}
