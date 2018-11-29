6ab27be1ca473d2bb9555742ba483793e6e44b9d
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-11-29 14:56:06 +0100

    Add files via upload

---
+++ CS-ASP_054/CS-ASP_054/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS_ASP_054
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public CharacterType Type { get; set; }
+
+    }
+
+    public enum CharacterType
+    {
+        Wizard,
+        Fighter,
+        Monster
+    }
+}
